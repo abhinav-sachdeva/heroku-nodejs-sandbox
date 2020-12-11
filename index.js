444aaf3ae3e6fb4fd6af44a403889ab19190c861
unknown <happy257@gmail.com>
2020-12-11 13:00:27 +0530

    removed logger

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const conf = require("./config/dev");
 
 // Setup logger
-const log = require("./logger")();
+// const log = require("./logger")();
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
-  logger: log,
+  // logger: log,
   maxParamLength: 50,
   onProtoPoisoning: "remove"
 });
@@ -47,10 +47,10 @@ routes.forEach((route, index) => {
 const start = async () => {
   try {
     await fastify.listen(conf.PORT, "127.0.0.1");
-    log.info(`server listening on ${fastify.server.address().port}`);
+    // log.info(`server listening on ${fastify.server.address().port}`);
     console.log(`In your browser navigate to: http://127.0.0.1:3000/documentation`)
   } catch (err) {
-    log.error(err);
+    // log.error(err);
     console.log(`Error listening on port ${conf.PORT}`)
     if (err.code === "EADDRINUSE")
       console.log(`Port ${conf.PORT} is being used by another application. Modify port in config/dev.js and re-lauch`)
